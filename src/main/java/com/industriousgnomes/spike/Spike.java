bca0189239c10bfcf4d17ff99a2d65f4c0b26e47
J.R. Titko <j.r.titko.professional@gmail.com>
2017-08-26 13:05:04 -0400

    add spike to see if spock is configured correctly

--- src/main/java/com/industriousgnomes/spike/Spike.java
+++ src/main/java/com/industriousgnomes/spike/Spike.java
@@ -10,4 +10,12 @@ public class Spike {
     public boolean testMe() {
         return injectMe.execute();
     }
+
+    public int sumNumbers(int max) {
+        int sum = 0;
+        for (int i = 1; i <= max; i++) {
+            sum += i;
+        }
+        return sum;
+    }
 }
--- src/test/groovy/com/industriousgnomes/SpikeTest.groovy
+++ src/test/groovy/com/industriousgnomes/SpikeTest.groovy
@@ -4,6 +4,7 @@ import com.industriousgnomes.spike.InjectMe
 import com.industriousgnomes.spike.Spike
 import spock.lang.Specification
 import spock.lang.Subject
+import spock.lang.Unroll
 
 /**
  * Created by jrtitko1 on 8/6/17.
@@ -15,11 +16,14 @@ class SpikeTest extends Specification {
 
     InjectMe injectMe = Mock()
 
+    def setup() {
+        spike = new Spike(
+                injectMe: injectMe
+        )
+    }
+
     def "Spike test to make sure spock works"() {
         given:
-            spike = new Spike(
-                    injectMe: injectMe
-            )
             injectMe.execute() >> true
 
         when:
@@ -27,5 +31,20 @@ class SpikeTest extends Specification {
 
         then:
             result
+    }
+
+    @Unroll
+    def "Sum numbers from 1 to #max"() {
+        when:
+            def sum = spike.sumNumbers(max);
+
+        then:
+            sum == solution
+
+
+        where:
+            max | solution
+            10  | 55
+            20  | 210
     }
 }
